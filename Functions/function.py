604c9d7abd6c6498676de596a46e3fe9d2506834
kiranit.mca <kiranit.mca@gmail.com>
2026-04-09 15:11:10 +0530

    functions

--- Functions/function.py
+++ Functions/function.py
@@ -1,5 +1,23 @@
 
-def learner(name):
-    print(f"{name} is learning Python.")
+# def learner(name):
+#     print(f"{name} is learning Python.")
 
-learner("Alice")
+# learner("Alice")
+
+
+# def pet(name,animal_type):
+#     print(f"{name} is a {animal_type}.")
+# pet("Buddy", "dog")
+
+# Variable length arguments
+
+def sum_numbers(*numbers):
+    total = 0
+    for n in numbers:
+        total += n
+    return total
+
+sum1 = sum_numbers(1, 2, 3)
+sum2 = sum_numbers(4, 5)
+print(f"Sum 1: {sum1}")
+print(f"Sum 2: {sum2}")
